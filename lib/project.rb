babe769e4b314ad7603cdaf56c8c41884fa6b738
Kristian Joos <kjoos22@gmail.com>
2020-10-06 00:34:19 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 class Project
-  attr_accessor :backed_projects
+  attr_accessor :backers
   attr_reader :title
   
   def initialize(title)
     @title = title
-    @backed_projects = []
+    @backers = []
   end
 
 end
